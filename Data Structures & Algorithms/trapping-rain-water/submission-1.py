e78df2733a6b0ad8354f23fbff29d725ce494b05
Arthur De Rouck <arthr.drck@gmail.com>
2026-08-25 21:42:04 +0800

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = list(accumulate(height,max))
+        right = list(accumulate(height[::-1],max))[::-1]
+        output=0
+        for i in range(len(left)):
+            output+=min(left[i],right[i])-height[i]
+        return output
